9ac38e31c291e34d26d3a59f2fc8228b90cd72af
gongbo <79492851@qq.com>
2024-07-11 11:33:30 +0800

    2024/7/11

--- app/src/main/java/com/example/forthhook/XposedHookDemo.java
+++ app/src/main/java/com/example/forthhook/XposedHookDemo.java
@@ -1,8 +1,6 @@
-package com.example.forthhook;//package com.example.forthhook;
-//
-//public class XpodesHookDemo {
-//}
+package com.example.forthhook;
 
+import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -15,18 +13,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedHookDemo implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        Log.d("two","11111111111111111111111111111111111111");
         // Hook into the package's code and modify its behavior here
         if (lpparam.packageName.equals("com.example.secondnoactivity")) {
+            Log.d("two","22222222222222222222222222222222222222");
             XposedBridge.log(lpparam.packageName+" has Hooked!");
             Class clazz = lpparam.classLoader.loadClass(
-                    "SecondNoActivity"
+                    "com.example.secondnoactivity.SecondNoActivity"
             );
-            XposedHelpers.findAndHookMethod(clazz,"generateRandomNumber", new XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(clazz,"generateRandomNumber", String.class, new XC_MethodHook() {
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     super.beforeHookedMethod(param);
                     XposedBridge.log("input :" +param.args[0]);
                 }
-            })
+                protected  void afterHookedMethod(MethodHookParam param) throws Throwable{
+                    param.setResult(666);
+                }
+            });
         }
 
     }
